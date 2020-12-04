83d567151d9309ecd9334a3b79d3ff1a23d0bf26
Richard <richardmenning@googlemail.com>
2020-12-04 16:52:50 +0100

    Initial Commit

---
+++ TouchBarDemo/TouchBarDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchBarDemoApp.swift
+//  TouchBarDemo
+//
+//  Created by Richard Menning on 04.12.20.
+//
+
+import SwiftUI
+
+@main
+struct TouchBarDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

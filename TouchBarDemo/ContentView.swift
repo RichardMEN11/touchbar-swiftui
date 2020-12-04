3d5d30ec092eb0f65f9a3bb5df42697b421dcd89
Richard <richardmenning@googlemail.com>
2020-12-04 16:59:50 +0100

    First attempt

--- TouchBarDemo/ContentView.swift
+++ TouchBarDemo/ContentView.swift
@@ -7,10 +7,34 @@
 
 import SwiftUI
 
+// important to know: https://stackoverflow.com/questions/59919050/how-can-i-display-touch-bar-buttons-using-swiftui
+// TODO: Always show touchbar element
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .focusable()
+                       .touchBar {
+                           Button(action: {
+                            print("GO!")
+                           }) {
+                               Text("▶️").padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                           }
+                        Button(action: {
+                            print("START")
+                        }) {
+                            Text("⏸").padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                        }
+                        Button(action: {
+                            print("STOP")
+                        }) {
+                            Text("⏹").padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                        }
+                        Text("20:00")
+                        Text("Projektname").padding().foregroundColor(.blue)
+                   }
             .padding()
+        
     }
 }
 
